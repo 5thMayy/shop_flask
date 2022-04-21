cdae44b70f0f3bafb776bcab693c9d1af94e1d6f
5thMayy <nguyendinhloc502@gmail.com>
2022-04-22 02:32:47 +0700

    Register

--- shop/admin/routes.py
+++ shop/admin/routes.py
@@ -1,13 +1,25 @@
 from turtle import title
-from flask import render_template, session, request, redirect, url_for
+from flask import render_template, session, request, redirect, url_for, flash
 
-from shop import app, db
-
+from shop import app, db, bcrypt
+from .forms import RegistrationForm
+from .models import User
+import os
 @app.route('/')
 def home():
-    return "Home page of your shop"
+    return render_template('admin/index.html', title="Admin Page")
 
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
-    return render_template('admin/register.html', title = "Register User")
+    form = RegistrationForm(request.form)
+    if request.method == 'POST' and form.validate():
+        hash_password = bcrypt.generate_password_hash(form.password.data)
+        user = User(name=form.name.data,username=form.username.data,
+        email=form.email.data,
+                    password=hash_password)
+        db.session.add(user)
+        db.session.commit()
+        flash(f' Welcome {form.name.data} Thanks for registering', 'success')
+        return redirect(url_for('home'))
+    return render_template('admin/register.html', form=form, title="Registeration page")
