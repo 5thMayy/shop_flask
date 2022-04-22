4c601d8a5661f1a45d2ab65b77373dd465ed36a9
5thMayy <nguyendinhloc502@gmail.com>
2022-04-22 13:34:06 +0700

    login form

--- shop/admin/routes.py
+++ shop/admin/routes.py
@@ -2,7 +2,7 @@ from turtle import title
 from flask import render_template, session, request, redirect, url_for, flash
 
 from shop import app, db, bcrypt
-from .forms import RegistrationForm
+from .forms import RegistrationForm, LoginForm
 from .models import User
 import os
 @app.route('/')
@@ -23,3 +23,17 @@ def register():
         flash(f' Welcome {form.name.data} Thanks for registering', 'success')
         return redirect(url_for('home'))
     return render_template('admin/register.html', form=form, title="Registeration page")
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    form = LoginForm(request.form)
+    if request.method == "POST" and form.validate():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            session['email'] = form.email.data
+            flash(f'welcome {form.email.data} you are logedin now','success')
+            return redirect(request.args.get('next') or url_for('admin'))
+        else:
+            flash(f'Wrong email and password', 'success')
+
+    return render_template('admin/login.html',form=form ,title='Login page')
