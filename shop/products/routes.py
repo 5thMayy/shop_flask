a0d4e98021c32fdf6581bd21c9bbd23f8f746004
5thMayy <nguyendinhloc502@gmail.com>
2022-04-22 14:32:21 +0700

    Select brands and categories in products

--- shop/products/routes.py
+++ shop/products/routes.py
@@ -1,7 +1,7 @@
 from flask import redirect, render_template,session, request, url_for, flash
 from shop import app,db
 from .models import Brand, Category
-
+from .forms import Addproducts
 
 
 @app.route('/addbrand',methods=['GET','POST'])
@@ -31,3 +31,27 @@ def addcat():
         return redirect(url_for('addcat'))
 
     return render_template('products/addbrand.html')
+
+@app.route('/addproduct', methods=['GET','POST'])
+def addproduct():
+    form = Addproducts(request.form)
+    brands = Brand.query.all()
+    categories = Category.query.all()
+    # if request.method=="POST"and 'image_1' in request.files:
+    #     name = form.name.data
+    #     price = form.price.data
+    #     discount = form.discount.data
+    #     stock = form.stock.data
+    #     colors = form.colors.data
+    #     desc = form.discription.data
+    #     brand = request.form.get('brand')
+    #     category = request.form.get('category')
+    #     image_1 = photos.save(request.files.get('image_1'), name=secrets.token_hex(10) + ".")
+    #     image_2 = photos.save(request.files.get('image_2'), name=secrets.token_hex(10) + ".")
+    #     image_3 = photos.save(request.files.get('image_3'), name=secrets.token_hex(10) + ".")
+    #     addproduct = Addproduct(name=name,price=price,discount=discount,stock=stock,colors=colors,desc=desc,category_id=category,brand_id=brand,image_1=image_1,image_2=image_2,image_3=image_3)
+    #     db.session.add(addproduct)
+    #     flash(f'The product {name} was added in database','success')
+    #     db.session.commit()
+    #     return redirect(url_for('admin'))
+    return render_template('products/addproduct.html', title='Add a Product', form = form, brands=brands, categories=categories)
